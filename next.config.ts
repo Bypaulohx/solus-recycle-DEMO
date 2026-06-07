432c2dacfeff644de3d8941d7af4fb3ce9361fea
Paulo Henrique <paulo.britoouril@gmail.com>
2026-06-07 17:42:46 -0300

    configs github next

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/solus-recycle-DEMO" : "",
   images: {
     unoptimized: true,
   },
